5a85fdc46919a316b52d2c4ba8e5b0f891066a3c
David Polo <davidpolocoffee@gmail.com>
2025-04-05 08:17:02 -0500

    Corregido: variables de entorno con  process.env.

--- connection.js
+++ connection.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
+
 const connection = mysql.createPool({
-    host: process.env.HOSTDB ||'localhost',
-    user: process.USERDB || 'root',
-    database: process.DB || 'login',
-    password: process.PASSWORDDB || '',
+    host: process.env.HOSTDB || 'localhost',
+    user: process.env.USERDB || 'root',       // Corregido: process.env.USERDB
+    database: process.env.DB || 'login',      // Corregido: process.env.DB
+    password: process.env.PASSWORDDB || '',   // Corregido: process.env.PASSWORDDB
     port: process.env.PORTDB || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     connectTimeout: 20000
-    });
+});
 
 module.exports = connection;
